05606cdfceecc0bdd68d2d64ca63074f7d11d54e
Julien Lemaire <julien.lemaire@etu.unice.fr>
2018-11-20 13:47:46 +0100

    Display adresses when lanching server

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const app = express();
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const os = require('os');	
 const prompt = require('prompt');
 
-server.listen(8080);
+server.listen(8080, function() {
+	// Display available adresses
+	const ifaces = os.networkInterfaces();
+	console.log("Running server");
+	console.log("Available on:");
+	Object.keys(ifaces).forEach(function (dev) {
+		ifaces[dev].forEach(function (details) {
+			if (details.family === 'IPv4') {
+				console.log(('  http://' + details.address + ':8080'));
+			}
+		});
+	});
+});
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + "/index.html");
