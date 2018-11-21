8bf162786f4999dc9d4f803d0c22486f3311c961
Julien Lemaire <julien.lemaire@etu.unice.fr>
2018-11-21 19:19:20 +0100

    Adapt tokens position on case

--- client/containers/Grid.js
+++ client/containers/Grid.js
@@ -2,6 +2,7 @@ module.exports = class Grid {
     constructor(gridLength, scene) {
         this.gridLength = gridLength;
         this.gridContainer = scene.add.container(0, scene.game.config.height / 2);
+        this.scene = scene;
         
         const gridCaseWidth = scene.game.config.width / this.gridLength;
         for (let i = 0 ; i < gridLength ; i++) {
@@ -15,6 +16,11 @@ module.exports = class Grid {
     }
 
     addToken(token, position) {
+        console.log(this.gridContainer.list[position]);
+        if (this.gridContainer.list[position].list.length > 1 ) { // there is already a token
+            token.setY(token.y - this.scene.game.config.height / 12);
+            this.gridContainer.list[position].list[1].setY(this.gridContainer.list[position].list[1].y + this.scene.game.config.height / 12);
+        }
         this.gridContainer.list[position].add(token);
     }
 }
