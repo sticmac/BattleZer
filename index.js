3fb27c34986d613f65f93e3f4c87c3357092857a
Julien Lemaire <julien.lemaire@etu.unice.fr>
2018-11-21 14:46:24 +0100

    Update main server

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const os = require('os');	
-const prompt = require('prompt');
+
+const StandaloneGame = require('./server/game_engine/StandaloneGame');
+const DistributedGame = require('./server/game_engine/DistributedGame');
 
 server.listen(8080, function() {
 	// Display available adresses
@@ -27,21 +29,96 @@ app.get('/', function (req, res) {
 app.use('/assets', express.static('assets'));
 app.use('/client', express.static('client'));
 
-io.on('connection', function (socket) {
-	console.log("connection");
-	socket.emit('client', {data: "Data recieved!"});
-	prompt.start();
-	sendUserContentSocket(socket);
-});
+io.on('connection', onConnect);
 
-function sendUserContentSocket(socket) {
 
-	prompt.get('caseId', function(err, result) {
-		if (result.caseId !== 'exit') {
-			socket.emit('move', {caseId: result.caseId});
-			sendUserContentSocket(socket);
-		}
-	});
+let games = [];
 
+
+function onConnect(socket) {
+
+    socket.on('start game', function (p) {
+        //ce socket correspond à la table
+
+        if (!isAlreadyInGame(socket.id)) {
+
+            let game;
+            let roomName = 'gameroom#' + (games.length + 1);
+            socket.join(roomName);
+
+            if (p.type === 'standalone') {
+                game = new StandaloneGame(roomName, p.players, socket.id, io);
+            } else {
+                game = new DistributedGame(roomName, p.players, socket.id, io);
+            }
+
+            games.push(game);
+        }
+
+    });
+
+    socket.on('join game', function (m) {
+        if (socket.rooms.hasOwnProperty(m.game)) {
+            //le socket est deja dans une game
+            socket.emit('chat message', {code: 401, message: 'client already associated to a game'})
+        } else {
+            //le socket n'est pas encore dans une game
+            let game = getGameByName(m.game);
+            if (game) {
+                if (game.type === 'standalone') {
+                    socket.emit('chat message', {
+                        code: 402,
+                        message: 'game is standalone mode, no other client allowed'
+                    });
+                } else {
+                    socket.join(m.game);
+                    game.addPlayer(socket.id)
+                }
+            } else socket.emit('chat message', {code: 403, message: 'requested game does not exists'})
+        }
+    });
+
+    socket.on('close game', function (m) {
+        let game = getGameByName(m)
+        if (game) game.closeGame();
+    })
+
+
+    socket.on('reset games', function (m) {
+        console.log('removing all current game (' + games.length + ')');
+        for (let i in games) {
+            let g = games[i];
+            for (let j in g.players) {
+                let p = g.players[j]
+                io.to(p.id).emit('chat message', {code: 404, message: g.name + ' is over'})
+
+            }
+            io.to(g.tableId).emit('chat message', {code: 404, message: g.name + ' is over'})
+        }
+        games = [];
+    });
+
+    socket.on('chat message', function (m) {
+        io.emit('chat message', m);
+    });
 }
 
+
+function getGameByName(n) {
+    let game = null;
+    for (let i in games) {
+        if (games[i].name === n) {
+            game = games[i];
+        }
+    }
+    return game;
+}
+
+function isAlreadyInGame(id) {
+    for (let i in games) {
+        if (games[i].tableId === id) {
+            io.to(id).emit('chat message', 'tu as deja commencé une game');
+            return true;
+        } else return false;
+    }
+}
