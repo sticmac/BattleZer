0ace0e62484470965993c5562539be372758410a
Julien Lemaire <julien.lemaire@etu.unice.fr>
2018-11-29 16:35:15 +0100

    Play round in distributed

--- client/scenes/DistributedGameScene.js
+++ client/scenes/DistributedGameScene.js
@@ -1,4 +1,5 @@
 const GameScene = require('./GameScene');
+const Round = require('../round/Round');
 
 module.exports = class DistributedGameScene extends GameScene {
     constructor() {
@@ -6,8 +7,19 @@ module.exports = class DistributedGameScene extends GameScene {
     }
 
     choiceStep(players) {
-        
+
     }
 
-
+    runRound() {
+        // clear game space
+        this.playersIds.forEach((id) => {
+            this.players[id].showAttack.undraw();
+        });
+        if (this.round === null) {
+            this.round = new Round(this.gameId, this.players, this.socket);
+        }
+        this.round.start(0, this.lastChosenAttacks);
+        this.lastPlayedIndex = 0; // player 0 starts
+        this.roundStep = true;
+    }
 }
