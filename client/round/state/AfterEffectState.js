2128650b7f186568e774aca409384890041ea170
Julien Lemaire <julien.lemaire@etu.unice.fr>
2018-12-14 10:38:56 +0100

    No choice for heal on standalone

--- client/round/state/AfterEffectState.js
+++ client/round/state/AfterEffectState.js
@@ -11,26 +11,37 @@ module.exports = class AfterEffectState extends RoundState {
         action['range'] = data.attack.actions.after[0].value;
         action['codes'] = data.attack.actions.after;
         let status = 'Effet d\'après attaque';
-
-        choice.draw(action, initPosition, status);
-        this.context.players[data.id].changeInteractContainer(choice.container);
-        choice.readyButton.on('pointerdown', () => {
-            if (choice.grid.choice !== null) {
-                choice.grid.actions.codes.forEach(c => {
-                    this.context.socket.emit('player effect', {
-                        game: game,
-                        attack: {
-                            player: data.id,
-                            action: c.action,
-                            value: choice.grid.choice
-                        }
+        
+        if (action.codes[0].action === "heal") { //Works only with one effect for after state
+            this.context.socket.emit('player effect', {
+                game: game,
+                attack: {
+                    player: data.id,
+                    action: action.codes[0].action,
+                    value: action.codes[0].value
+                }
+            });
+        } else {
+            choice.draw(action, initPosition, status);
+            this.context.players[data.id].changeInteractContainer(choice.container);
+            choice.readyButton.on('pointerdown', () => {
+                if (choice.grid.choice !== null) {
+                    choice.grid.actions.codes.forEach(c => {
+                        this.context.socket.emit('player effect', {
+                            game: game,
+                            attack: {
+                                player: data.id,
+                                action: c.action,
+                                value: choice.grid.choice
+                            }
+                        });
+                        choice.undraw();
                     });
-                    choice.undraw();
-                });
-            } else {
-                console.log("after choice pas ok");
-            }
-        });
+                } else {
+                    console.log("after choice pas ok");
+                }
+            });
+        }
 
     }
 
--- client/round/state/BeforeEffectState.js
+++ client/round/state/BeforeEffectState.js
@@ -14,25 +14,36 @@ module.exports = class BeforeEffectState extends RoundState {
         action['codes'] = data.attack.actions.before;
         let status = 'Effet d\'avant attaque';
 
-        choice.draw(action, initPosition, status);
-        this.context.players[data.id].changeInteractContainer(choice.container);
-        choice.readyButton.on('pointerdown', () => {
-            if (choice.grid.choice !== null) {
-                choice.grid.actions.codes.forEach(c => {
-                    this.context.socket.emit('player effect', {
-                        game: game,
-                        attack: {
-                            player: data.id,
-                            action: c.action,
-                            value: choice.grid.choice
-                        }
+        if (action.codes[0].action === "heal") { //Works only with one effect for before state
+            this.context.socket.emit('player effect', {
+                game: game,
+                attack: {
+                    player: data.id,
+                    action: action.codes[0].action,
+                    value: action.codes[0].value
+                }
+            });
+        } else {
+            choice.draw(action, initPosition, status);
+            this.context.players[data.id].changeInteractContainer(choice.container);
+            choice.readyButton.on('pointerdown', () => {
+                if (choice.grid.choice !== null) {
+                    choice.grid.actions.codes.forEach(c => {
+                        this.context.socket.emit('player effect', {
+                            game: game,
+                            attack: {
+                                player: data.id,
+                                action: c.action,
+                                value: choice.grid.choice
+                            }
+                        });
+                        choice.undraw();
                     });
-                    choice.undraw();
-                });
-            } else {
-                console.log("before choice pas ok");
-            }
-        });
+                } else {
+                    console.log("before choice pas ok");
+                }
+            });
+        }
     }
 
     next() {
